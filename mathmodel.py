209579130e63451691d68479180aba29a896a312
KoschavtcevDaniel <112947715+KoschavtcevDaniel@users.noreply.github.com>
2024-02-23 09:55:54 +0300

    Update mathmodel.py

--- mathmodel.py
+++ mathmodel.py
@@ -1,13 +1,13 @@
-from matplotlib.pypplot import plot
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 with open('input.txt', 'r') as file:
-    a = float(file.readline().strip().split())
-    b = float(file.readline().strip().split())
-    eps = float(file.readline().strip())
-    l = float(file.readline().strip())
-
+    a, b, eps, l = 0, 10, 0.2, 0.5
+    x_val = np.arange(a, b, eps)
+    y_val = 2 * x_val ** 2 - 12 * x_val
     k = 0
-    f = lambda x: 2 * x**2 - 12 * x
+    f = lambda x: 2 * x ** 2 - 12 * x
     while True:
         y = (a + b - eps) / 2
         z = (a + b + eps) / 2
@@ -22,10 +22,12 @@ with open('input.txt', 'r') as file:
             break
         else:
             k += 1
-            
+
 print('Answer is')
 print(f'X is {x}, f(x) = {f(x)}')
-print(f'N is {2*(k+1)}')
+print(f'N is {2 * (k + 1)}')
 print(f'Interval is [{a}, {b}]')
 
-plot(y, x)
+plt.plot(x_val, y_val)
+plt.scatter(x, f(x), c='red')
+plt.show()
